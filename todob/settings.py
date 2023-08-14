ddc489a23e5d3e7cadd01f810b304fa10f74903a
saugatguni <saugat.guni@gmail.com>
2023-08-14 17:30:49 +0545

    django, backend part done

--- todob/settings.py
+++ todob/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todoapp'
+    'todoapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
